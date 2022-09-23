2cc706b228094d6a43e38f8ce938e459bebc711d
LLukas <114211184+LavaLukas@users.noreply.github.com>
2022-09-23 09:07:11 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let zBereich;
+
 function setup() {
-  createCanvas(600, 400);
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0);//Links oben
+  zBereich.style('z-index','-1'); //Verschiebung auf der z-Achse
 }
 
 function draw() {
-  background(220);
+ //background(0,100,200);
+ //background.style('z-index','-10');
+ noStroke;
+ if (mouseX < windowWidth / 2) {
+  fill(0,255,0,50);
+ }
+ else {
+  fill(255,0,0,50);
+ }
+ circle(mouseX,mouseY,20);
 }
